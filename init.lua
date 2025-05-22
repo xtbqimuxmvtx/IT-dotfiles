b158ada2d07706caaf1541547708e8d6c0b8d91a
xtbqimuxmvtx <112921562+xtbqimuxmvtx@users.noreply.github.com>
2025-05-22 17:11:05 +0930

    add TreeSitter; kickstart plugins & restored comments
    kickstart.plugins.autopairs
    & commented out indent_line; lint; gitsigns
    Add treesitter
    Add mini.nvim
    Add autoformat
    Add blink.cmp commented out (Ubuntu not on Nvim 0.10 yet)
    
    Added a new commenting format (end of Telescope) WIP

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.signcolumn = 'yes'
-vim.o.updatetime = 250
+vim.o.updatetime = 25
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -108,8 +108,222 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         builtin.find_files({ cwd = vim.fn.stdpath('config') })
         end, { desc = '[S]earch [N]eovim files' })
         end,
+      },    --=+$%#%$+= End Telescope.nvim configuration ###############
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
       },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+  },
+--  { -- Autocompletion
+--    'saghen/blink.cmp',
+--    event = 'VimEnter',
+--    version = '1.*',
+--    dependencies = {
+--      -- Snippet Engine
+--      {
+--        'L3MON4D3/LuaSnip',
+--        version = '2.*',
+--        build = (function()
+--          -- Build Step is needed for regex support in snippets.
+--          -- This step is not supported in many windows environments.
+--          -- Remove the below condition to re-enable on windows.
+--          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+--            return
+--          end
+--          return 'make install_jsregexp'
+--        end)(),
+--        dependencies = {
+--          -- `friendly-snippets` contains a variety of premade snippets.
+--          --    See the README about individual language/framework/plugin snippets:
+--          --    https://github.com/rafamadriz/friendly-snippets
+--          -- {
+--          --   'rafamadriz/friendly-snippets',
+--          --   config = function()
+--          --     require('luasnip.loaders.from_vscode').lazy_load()
+--          --   end,
+--          -- },
+--        },
+--        opts = {},
+--      },
+--      'folke/lazydev.nvim',
+--    },
+--    --- @module 'blink.cmp'
+--    --- @type blink.cmp.Config
+--    opts = {
+--      keymap = {
+--        -- 'default' (recommended) for mappings similar to built-in completions
+--        --   <c-y> to accept ([y]es) the completion.
+--        --    This will auto-import if your LSP supports it.
+--        --    This will expand snippets if the LSP sent a snippet.
+--        -- 'super-tab' for tab to accept
+--        -- 'enter' for enter to accept
+--        -- 'none' for no mappings
+--        --
+--        -- For an understanding of why the 'default' preset is recommended,
+--        -- you will need to read `:help ins-completion`
+--        --
+--        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+--        --
+--        -- All presets have the following mappings:
+--        -- <tab>/<s-tab>: move to right/left of your snippet expansion
+--        -- <c-space>: Open menu or open docs if already open
+--        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+--        -- <c-e>: Hide menu
+--        -- <c-k>: Toggle signature help
+--        --
+--        -- See :h blink-cmp-config-keymap for defining your own keymap
+--        preset = 'default',
+--
+--        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+--        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+--      },
+--
+--      appearance = {
+--        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+--        -- Adjusts spacing to ensure icons are aligned
+--        nerd_font_variant = 'mono',
+--      },
+--
+--      completion = {
+--        -- By default, you may press `<c-space>` to show the documentation.
+--        -- Optionally, set `auto_show = true` to show the documentation after a delay.
+--        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+--      },
+--
+--      sources = {
+--        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+--        providers = {
+--          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+--        },
+--      },
+--
+--      snippets = { preset = 'luasnip' },
+--
+--      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+--      -- which automatically downloads a prebuilt binary when enabled.
+--      --
+--      -- By default, we use the Lua implementation instead, but you may enable
+--      -- the rust implementation via `'prefer_rust_with_warning'`
+--      --
+--      -- See :h blink-cmp-config-fuzzy for more information
+--      fuzzy = { implementation = 'lua' },
+--
+--      -- Shows a signature help window while you type arguments for a function
+--      signature = { enabled = true },
+--    },
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+--  },
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+    require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 })
+  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+  --
+  --  Here are some example plugins that I've included in the Kickstart repository.
+  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+  --
       -- Other plugins like LSP, colorschemes, etc., go here...
 require("oil").setup({
   -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
